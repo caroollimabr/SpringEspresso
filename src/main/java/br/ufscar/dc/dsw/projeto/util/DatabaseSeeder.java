8582ce3d0d57e56944b866b116cb845c008ae531
lksalto <saltolks@gmail.com>
2025-12-08 20:33:45 -0300

    Ajuste tela de bugs - voltar ao relatório

--- src/main/java/br/ufscar/dc/dsw/projeto/util/DatabaseSeeder.java
+++ src/main/java/br/ufscar/dc/dsw/projeto/util/DatabaseSeeder.java
@@ -46,7 +46,7 @@ public class DatabaseSeeder implements CommandLineRunner {
     @Override
     public void run(String... args) {
 
-        boolean popula = true;  // teste
+        boolean popula = false;  // teste
 
         if (!popula) {
             System.out.println("População do banco de dados desativada.");
